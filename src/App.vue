4b50aad0977862213b8e084cc0b55c05c73b3a6d
William Anthony <tony@burrus.me>
2023-01-11 18:32:58 -0800

    foundation

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <Landing/>
+</template>
+
+<script>
+import Landing from './components/Landing.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Landing
+  }
+}
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;900&display=swap");
+
+body {
+  background: linear-gradient(to right top, #F2D685, #F37335);
+    background-attachment: fixed;
+}
+
+#app {
+  font-family: "Montserrat", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  margin: 50px;
+}
+</style>
---
+++ src/components/Landing.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="landing">
+    <h2>Noaspot</h2>
+    <h1>Coming Soon</h1>
+    <p>Add your email below to </p>
+    <p>be notified when we launch.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Landing',
+}
+</script>
+
+<style scoped>
+.landing {
+  color: #FFF;
+}
+
+h1 {
+  font-size: 5em;
+  font-weight: 900;
+}
+
+p {
+  margin: 5px;
+  font-weight: 600;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>

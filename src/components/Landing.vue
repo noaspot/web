5d889f3d14acdb84e8a677f7f7bb1fcc0d6c6714
William Anthony <tony@burrus.me>
2023-01-12 00:59:53 -0800

    copyright

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -4,6 +4,7 @@
     <h1>Coming Soon</h1>
     <p>Add your email below to </p>
     <p>be notified when we launch.</p>
+    <p id="copyright">© 2023 Noaspot LLC</p>
   </div>
 </template>
 
@@ -18,6 +19,10 @@ export default {
   color: #FFF;
 }
 
+h2 {
+  letter-spacing: 2px;
+}
+
 h1 {
   font-size: 5em;
   font-weight: 900;
@@ -28,15 +33,8 @@ p {
   font-weight: 600;
 }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+#copyright {
+  font-size: 0.85em;
+  font-weight: 400;
 }
 </style>
